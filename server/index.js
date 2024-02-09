1594f25d58c0ea788ba4a43092478974c1c0f3c6
arashjp8 <arashjafarpour723@gmail.com>
2024-02-09 09:46:34 +0330

    [checkpoint] UserSchema added

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import { register } from "./routes/auth.js";
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -33,6 +34,13 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
+
+// ROUTES WITH FILES
+app.post(
+  "/auth/register" /*route*/,
+  upload.single("picture") /*middleware*/,
+  register /*controller*/,
+);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
