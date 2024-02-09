c00c6f5120ee2f7bce15bd946b22c553f1a04156
arashjp8 <arashjafarpour723@gmail.com>
2024-02-09 10:55:33 +0330

    [checkpoint] login functionality added

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -44,3 +44,26 @@ export const register = async (req, res) => {
     res.status(500).json({ message: err.message });
   }
 };
+
+// LOGGING IN
+export const login = async (req, res) => {
+  try {
+    // get user input
+    const { email, password } = req.body;
+
+    // find user
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: "User does not exist" });
+
+    // check if password is correct
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid password" });
+
+    // create token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
